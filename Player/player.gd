c100613aba6d775503fb7f01adf52027d0408057
Fabio Silva <fabio.silva99@hotmail.com>
2026-07-04 17:45:48 +0100

    Implemented camera vertical rotation and camera smoothing

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = 4.5
 
 var mouse_motion := Vector2.ZERO
 
+@onready var camera_pivot: Node3D = $CameraPivot
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -37,12 +39,16 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		mouse_motion = -event.relative * 0.001
 	
-	if event.is_action("ui_cancel"):
+	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
 		
 		
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x, -90.0, 90.0
+	)
 	mouse_motion = Vector2.ZERO
 	
